37196bd226dc3eb9a31ac394f56425cde6f40bb5
Mayank Sharma <mayanksh@gmail.com>
2020-06-12 15:28:34 -0400

    Jta config

--- src/main/java/com/sample/config/JpaConfig.java
+++ src/main/java/com/sample/config/JpaConfig.java
@@ -5,23 +5,22 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.support.SharedEntityManagerBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@ImportResource(value = { "classpath:spring/spring-jbpmService.xml" })
+@ImportResource(value = { "classpath:spring/spring-jbpmService.xml",
+						  "classpath:spring/spring-domain.xml" 
+						}
+				)
 public class JpaConfig {
 
 	@Value("${dataSource.url}")
@@ -54,7 +53,7 @@ public class JpaConfig {
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 		em.setDataSource(dataSource());
-		em.setPersistenceXmlLocation("classpath*:META-INF/jbpm-persistence-JPA2.xml");		
+		em.setPersistenceXmlLocation("classpath*:META-INF/jbpm-persistence-jta.xml");		
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		em.setJpaVendorAdapter(vendorAdapter);
 		em.setJpaProperties(additionalProperties());
@@ -70,22 +69,5 @@ public class JpaConfig {
 		properties.setProperty("hibernate.default_schema", "WF");
 		return properties;
 	}
-
-	@Bean(name = "sampleTransactionManager")
-	@Primary
-	public PlatformTransactionManager transactionManager() {
-		JpaTransactionManager transactionManager = new JpaTransactionManager();
-		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-		transactionManager.setDataSource(dataSource());
-		transactionManager.setNestedTransactionAllowed(true);
-		return transactionManager;
-	}
-
-	@Bean(name = "sampleEntityManager")
-	@Primary
-	public SharedEntityManagerBean entityManager() {
-		SharedEntityManagerBean entityManager = new SharedEntityManagerBean();
-		entityManager.setEntityManagerFactory(entityManagerFactory().getObject());
-		return entityManager;
-	}
+	
 }
